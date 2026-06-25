e25f59d9253d16df1c8dcb150cd325c19f7db45a
santanu jana <sjchem@gmail.com>
2026-06-25 11:11:20 +0100

    Package Python dependencies for Azure deploy

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+export PYTHONPATH="/home/site/wwwroot/.python_packages/lib/site-packages:${PYTHONPATH:-}"
+
 gunicorn -w "${WEB_CONCURRENCY:-2}" \
   -k uvicorn.workers.UvicornWorker \
   -b 0.0.0.0:"${PORT:-8000}" \
